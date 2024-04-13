5526a998ec44e097fe1c6e247b7e64dfedc64b50
anıl sezgin <anlsezgin3@gmail.com>
2024-04-13 17:14:07 +0300

    #2 Server-side application added

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>
+{
+    res.render("index.ejs");
+})
+
+app.listen(port,(req,res)=>
+{
+    console.log(`Server running on port: ${port}`);
+})
